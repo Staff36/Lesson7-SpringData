d1e599e2a7013b09b77e3169864fd0cabbc38dc0
TroninDmitry <dima-tronin@mail.ru>
2021-07-08 23:45:24 +0300

    Pagination added

--- src/main/java/ru/tronin/springdata/controllers/ProductsController.java
+++ src/main/java/ru/tronin/springdata/controllers/ProductsController.java
@@ -1,6 +1,8 @@
 package ru.tronin.springdata.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,6 +12,10 @@ import ru.tronin.springdata.models.Product;
 import ru.tronin.springdata.services.ProductService;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 @Controller
@@ -24,20 +30,35 @@ public class ProductsController {
     public String index(Model model,
                         @RequestParam(name = "min", required = false) Double min,
                         @RequestParam(name = "max", required = false) Double max,
-                        @RequestParam(name = "partName", required = false) String partName){
-        Double minimumCost = productService.getMinimumCost();
-        Double maximumCost = productService.getMaximumCost();
-        min = min == null ? minimumCost : min;
-        max = max == null ? maximumCost : max;
-        partName = partName == null ? "" : partName;
+                        @RequestParam(name = "partName", required = false) String partName,
+                        @RequestParam("page") Optional<Integer> page,
+                        @RequestParam("size") Optional<Integer> size){
+            Double minimumCost = productService.getMinimumCost();
+            Double maximumCost = productService.getMaximumCost();
+            min = min == null ? minimumCost : min;
+            max = max == null ? maximumCost : max;
+            partName = partName == null ? "" : partName;
+            int currentPage = page.orElse(1);
+            int pageSize = size.orElse(12);
+            Page<Product> productPage = productService.findPaginatedProducts(PageRequest.of(currentPage - 1, pageSize), min, max, partName);
 
-        model.addAttribute("minCost", minimumCost);
-        model.addAttribute("maxCost", maximumCost);
-        model.addAttribute("partName", partName);
-        model.addAttribute("from", min);
-        model.addAttribute("to", max);
-        model.addAttribute("prods", productService.getProducts(min, max, partName));
-        return "products/products";
+            model.addAttribute("prodPage", productPage);
+
+            int totalPages = productPage.getTotalPages();
+
+            if(totalPages > 0){
+                List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                        .boxed()
+                        .collect(Collectors.toList());
+                model.addAttribute("pageNumbers", pageNumbers);
+            }
+            model.addAttribute("minCost", minimumCost);
+            model.addAttribute("maxCost", maximumCost);
+            model.addAttribute("partName", partName);
+            model.addAttribute("from", min);
+            model.addAttribute("to", max);
+
+            return "products/products";
     }
 
     @GetMapping("/{id}")
@@ -87,5 +108,7 @@ public class ProductsController {
         productService.deleteById(id);
         return "redirect:/products";
     }
+
+
 
 }
--- src/main/java/ru/tronin/springdata/services/ProductService.java
+++ src/main/java/ru/tronin/springdata/services/ProductService.java
@@ -3,11 +3,16 @@ package ru.tronin.springdata.services;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import ru.tronin.springdata.exceptions.NoEntityException;
 import ru.tronin.springdata.models.Product;
 import ru.tronin.springdata.repositories.ProductRepository;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -29,6 +34,22 @@ public class ProductService {
             throw new NoEntityException("Entity with id =" + id + " not found");
         }
         return productRepository.getById(id);
+    }
+
+    public Page<Product> findPaginatedProducts(Pageable pageable, Double min, Double max, String namePart){
+        int pageSize = pageable.getPageSize();
+        int currentPage = pageable.getPageNumber();
+        int startItem = currentPage * pageSize;
+        List <Product> productsFromDB = getProducts(min, max, namePart);
+        List<Product> products;
+        if (productsFromDB.size() < startItem){
+            products = Collections.emptyList();
+        } else {
+            int endItem = Math.min(startItem + pageSize, productsFromDB.size());
+            products = productsFromDB.subList(startItem, endItem);
+        }
+
+        return new PageImpl<Product>(products, PageRequest.of(currentPage, pageSize), productsFromDB.size());
     }
 
     public void create(Product product) {
