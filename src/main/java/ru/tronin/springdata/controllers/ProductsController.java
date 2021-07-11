50099bbc353be71eb7461eabacf7ed80c09b9f5b
TroninDmitry <dima-tronin@mail.ru>
2021-07-11 17:56:32 +0300

    Switched on AngularJS. HT passed

--- src/main/java/ru/tronin/springdata/controllers/ProductsController.java
+++ src/main/java/ru/tronin/springdata/controllers/ProductsController.java
@@ -1,8 +1,13 @@
 package ru.tronin.springdata.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,7 +23,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 
-@Controller
+@RestController
 @RequestMapping("/products")
 public class ProductsController {
 
@@ -27,86 +32,67 @@ public class ProductsController {
 
 
     @GetMapping()
-    public String index(Model model,
-                        @RequestParam(name = "min", required = false) Double min,
-                        @RequestParam(name = "max", required = false) Double max,
-                        @RequestParam(name = "partName", required = false) String partName,
-                        @RequestParam("page") Optional<Integer> page,
-                        @RequestParam("size") Optional<Integer> size){
-            Double minimumCost = productService.getMinimumCost();
-            Double maximumCost = productService.getMaximumCost();
-            min = min == null ? minimumCost : min;
-            max = max == null ? maximumCost : max;
-            partName = partName == null ? "" : partName;
-            int currentPage = page.orElse(1);
-            int pageSize = size.orElse(12);
-            Page<Product> productPage = productService.findPaginatedProducts(PageRequest.of(currentPage - 1, pageSize), min, max, partName);
+    public Page<Product> index(Model model,
+                        @PageableDefault(size = 12) Pageable pageable,
+                        @RequestParam(name = "min_price", required = false) Double min,
+                        @RequestParam(name = "max_price", required = false) Double max,
+                        @RequestParam(name = "name_part", required = false) String partName){
 
-            model.addAttribute("prodPage", productPage);
-
-            int totalPages = productPage.getTotalPages();
-
-            if(totalPages > 0){
-                List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
-                        .boxed()
-                        .collect(Collectors.toList());
-                model.addAttribute("pageNumbers", pageNumbers);
-            }
-            model.addAttribute("minCost", minimumCost);
-            model.addAttribute("maxCost", maximumCost);
-            model.addAttribute("partName", partName);
-            model.addAttribute("from", min);
-            model.addAttribute("to", max);
-
-            return "products/products";
+            return productService.findPaginatedProducts(pageable, min, max, partName);
     }
 
-    @GetMapping("/{id}")
-    public String showProduct(@PathVariable Long id, Model model){
-    model.addAttribute("product", productService.getEntityById(id));
-    return "products/show";
-}
 
-    @GetMapping("/new")
-    public String newProduct(Model model){
-        model.addAttribute("prod", new Product());
-        return "products/new";
-     }
+
+//    @GetMapping("/{id}")
+//    public String showProduct(@PathVariable Long id, Model model){
+//    model.addAttribute("product", productService.getEntityById(id));
+//    return "products/show";
+//}
+
+//    @GetMapping("/new")
+//    public String newProduct(Model model){
+//        model.addAttribute("prod", new Product());
+//        return "products/new";
+//     }
 
     @PostMapping()
-    public String createProduct(@ModelAttribute("prod") @Valid Product product, BindingResult bindingResult) {
-        if(bindingResult.hasErrors()){
-            return "products/new";
+    public ResponseEntity<Object> createProduct(@RequestBody @Valid Product product, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()){
+            List<String> errors = bindingResult.getAllErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.toList());
+            return new ResponseEntity<>(errors, HttpStatus.OK);
         }
         productService.create(product);
-        return "redirect:/products";
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}/edit")
-    public String editProduct(Model model, @PathVariable Long id){
-        try {
-            model.addAttribute("prod", productService.getEntityById(id));
-        } catch (NoEntityException ignored){
-            return "products/notFound";
-        }
-        return "products/edit";
-    }
+//    @GetMapping("/{id}/edit")
+//    public String editProduct(Model model, @PathVariable Long id){
+//        try {
+//            model.addAttribute("prod", productService.getEntityById(id));
+//        } catch (NoEntityException ignored){
+//            return "products/notFound";
+//        }
+//        return "products/edit";
+//    }
 
-    @PatchMapping("/{id}")
-    public String updateProduct(@ModelAttribute("prod") @Valid Product product,
-                                BindingResult bindingResult,
-                                @PathVariable Long id){
-        if (bindingResult.hasErrors()){
-            return "products/edit";
-        }
-        productService.update(product); //Переделать
-        return "redirect:/products";
-    }
+//    @PatchMapping("/{id}")
+//    public ResponseEntity<Object> updateProduct(@RequestBody @Valid Product product,
+//                                BindingResult bindingResult){
+//        if (bindingResult.hasErrors()){
+//            List<String> errors = bindingResult.getAllErrors().stream()
+//                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+//                    .collect(Collectors.toList());
+//            return new ResponseEntity<>(errors, HttpStatus.OK);
+//        }
+//        productService.update(product);
+//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//    }
 
     @DeleteMapping("/{id}")
-    public String deleteProduct(@PathVariable Long id){
+    public void deleteProduct(@PathVariable Long id){
         productService.deleteById(id);
-        return "redirect:/products";
     }
 
 
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,62 @@
+angular.module('app', []).controller('indexController', function ($scope, $http) {
+    const contextPath = 'http://localhost:8080';
+
+    $scope.fillTable = function (page) {
+        $http({
+            url: contextPath + '/products',
+            method: 'GET',
+            params: {
+                min_price: $scope.filter ? $scope.filter.min_price : null,
+                max_price: $scope.filter ? $scope.filter.max_price : null,
+                name_part: $scope.filter ? $scope.filter.name_part : null,
+                page: page ? page - 1 : null,
+                size: $scope.filter ? $scope.filter.size : 5
+            }
+        }).then(function (response) {
+            $scope.ProductsList = response.data.content;
+            var pages = response.data.totalPages;
+            var pagination = fillArray(pages);
+            $scope.PagesList = pagination;
+        });
+    };
+
+    function fillArray(pages) {
+        var a = [];
+        for (var i = 1; i <= pages; i++) {
+            a.push(i);
+        }
+        return a;
+    }
+
+    $scope.deleteProductById = function (id){
+        $http.delete(contextPath + '/products/' + id, null).then(function (){
+            $scope.fillTable();
+        });
+
+    }
+
+    $scope.submitUpdateProduct = function () {
+        $http.post(contextPath + '/products', $scope.newProduct)
+            .then(function (response) {
+                $scope.newProduct = null;
+                $scope.fillTable();
+            });
+    };
+
+    // $scope.submitCreateNewProduct = function () {
+    //     $http.patch(contextPath + '/products', $scope.updatedProduct)
+    //         .then(function (response) {
+    //             $scope.newProduct = null;
+    //             $scope.fillTable();
+    //         });
+    // };
+    $scope.numberPages = [
+        { name: '5', value: 5 },
+        { name: '10', value: 10 },
+        { name: '20', value: 20}
+    ];
+
+
+    $scope.fillTable();
+});
+
